14e1e7a9fe1c774b3ea5b64d75e488e7621797ae
zaenal <zaenal@laku6.com>
2021-01-26 20:53:32 +0700

    change interactor into optional

--- Sources/CoreSDK/Presentation/GetListPresenter.swift
+++ Sources/CoreSDK/Presentation/GetListPresenter.swift
@@ -10,36 +10,42 @@ import SwiftUI
 import Combine
 
 public class GetListPresenter<Request, Response, Interactor: UseCase>: ObservableObject where Interactor.Request == Request, Interactor.Response == [Response] {
-    
-    private var cancellables: Set<AnyCancellable> = []
-    
-    private let _useCase: Interactor
-    
-    @Published public var list: [Response] = []
-    @Published public var errorMessage: String = ""
-    @Published public var isLoading: Bool = false
-    @Published public var isError: Bool = false
-    
-    public init(useCase: Interactor) {
-        _useCase = useCase
+  
+  private var cancellables: Set<AnyCancellable> = []
+  
+  private let _useCase: Interactor?
+  
+  @Published public var list: [Response] = []
+  @Published public var errorMessage: String = ""
+  @Published public var isLoading: Bool = false
+  @Published public var isError: Bool = false
+  
+  public init(useCase: Interactor?) {
+    _useCase = useCase
+  }
+  
+  public func getList(request: Request?) {
+    isLoading = true
+    if let useCase = _useCase {
+      _useCase.execute(request: request)
+        .receive(on: RunLoop.main)
+        .sink(receiveCompletion: { completion in
+          switch completion {
+          case .failure(let error):
+            self.errorMessage = error.localizedDescription
+            self.isError = true
+            self.isLoading = false
+          case .finished:
+            self.isLoading = false
+          }
+        }, receiveValue: { list in
+          self.list = list
+        })
+        .store(in: &cancellables)
+    } else {
+      self.errorMessage = "Usecase has no value"
+      self.isError = true
+      self.isLoading = false
     }
-    
-    public func getList(request: Request?) {
-        isLoading = true
-        _useCase.execute(request: request)
-            .receive(on: RunLoop.main)
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .failure(let error):
-                    self.errorMessage = error.localizedDescription
-                    self.isError = true
-                    self.isLoading = false
-                case .finished:
-                    self.isLoading = false
-                }
-            }, receiveValue: { list in
-                self.list = list
-            })
-            .store(in: &cancellables)
-    }
+  }
 }
