0538cab32ee710eb44d0f1c77188343964a2ff66
zaenal <zaenal@laku6.com>
2021-01-26 22:25:28 +0700

    add more error case

--- Sources/CoreSDK/Extension/CustomError+Ext.swift
+++ Sources/CoreSDK/Extension/CustomError+Ext.swift
@@ -8,25 +8,27 @@
 import Foundation
 
 public enum URLError: LocalizedError {
-
+  
   case invalidResponse
+  case invalidParameter
   case addressUnreachable(URL)
   
-    public var errorDescription: String? {
+  public var errorDescription: String? {
     switch self {
     case .invalidResponse: return "The server responded with garbage."
+    case .invalidParameter: return "Request is invalid."
     case .addressUnreachable(let url): return "\(url.absoluteString) is unreachable."
     }
   }
-
+  
 }
 
 public enum DatabaseError: LocalizedError {
-
+  
   case invalidInstance
   case requestFailed
   
-    public var errorDescription: String? {
+  public var errorDescription: String? {
     switch self {
     case .invalidInstance: return "Database can't instance."
     case .requestFailed: return "Your request failed."
