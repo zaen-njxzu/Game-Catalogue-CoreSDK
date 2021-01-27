19651fe2bbd517639de2513c1efddd58471551b8
zaenal <zaenal@laku6.com>
2021-01-27 07:23:01 +0700

    change back

--- Sources/CoreSDK/Presentation/GetListPresenter.swift
+++ Sources/CoreSDK/Presentation/GetListPresenter.swift
@@ -13,14 +13,14 @@ public class GetListPresenter<Request, Response, Interactor: UseCase>: Observabl
   
   private var cancellables: Set<AnyCancellable> = []
   
-  private let _useCase: Interactor
+  private let _useCase: Interactor?
   
   @Published public var list: [Response] = []
   @Published public var errorMessage: String = ""
   @Published public var isLoading: Bool = false
   @Published public var isError: Bool = false
   
-  public init(useCase: Interactor) {
+  public init(useCase: Interactor?) {
     _useCase = useCase
   }
   
