b64d6f8bb01669c9aac24c0fbec9dcaa2c69b06c
zaenal <zaenal@laku6.com>
2021-01-27 22:05:48 +0700

    add more method

--- Sources/CoreSDK/Domain/Interactor.swift
+++ Sources/CoreSDK/Domain/Interactor.swift
@@ -10,15 +10,15 @@ import Combine
 
 public struct Interactor<Request, Response, R: Repository>: UseCase
 where R.Request == Request, R.Response == Response {
-    
-    private let _repository: R
-    
-    public init(repository: R) {
-        _repository = repository
-    }
-    
-    public func execute(request: Request?) -> AnyPublisher<Response, Error> {
-        _repository.execute(request: request)
-    }
+  
+  private let _repository: R
+  
+  public init(repository: R) {
+    _repository = repository
+  }
+  
+  public func execute(request: Request?) -> AnyPublisher<Response, Error> {
+    _repository.execute(request: request)
+  }
 }
 
